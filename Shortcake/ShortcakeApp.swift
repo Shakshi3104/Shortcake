d95b98bd6430c97d3cb36bd20809694c808ac359
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-05-27 20:08:52 +0900

    Initial Commit

---
+++ Shortcake/ShortcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcakeApp.swift
+//  Shortcake
+//
+//  Created by MacBook Pro M1 on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct ShortcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
